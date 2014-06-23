e35640431079cee06faca8d41d2bcbc5a47164d9
milk0824 <aku_soku_zan_1bantai@hotmail.com>
2014-06-24 00:30:26 +0900

    Making Enemy base

--- background.cs
+++ background.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class background : MonoBehaviour {
-
+	private float speed = 0.01f;
+	private float count = 0f;
 	// Use this for initialization
 	void Start () {
 
@@ -10,6 +11,16 @@ public class background : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 pos = this.transform.position;
+		pos.y -= speed;
+		this.transform.position = pos;
+		count += speed;
+
+		if(this.transform.position.y <= -9.6f){
+			pos.y = 9.6f;
+			this.transform.position = pos;
+		}
+
+
 	}
 }
---
+++ bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+	// 移動速度を設定
+	// @param direction 角度
+	// @param speed 速さ
+	// @param rotationTrigger(bool)  rotate image
+	public void Create(float direction, float speed, bool rotationTrigger) {
+		Vector2 v;
+		//Rotate image
+		if(rotationTrigger) this.transform.Rotate (0,0,direction - 90);
+		
+		v.x = Mathf.Cos (Mathf.Deg2Rad * direction) * speed;
+		v.y = Mathf.Sin (Mathf.Deg2Rad * direction) * speed;
+		rigidbody2D.velocity = v;
+	}
+	
+	//if bullet go out of screen, delete it
+	void OnTriggerEnter2D(Collider2D c){
+		int layer = LayerMask.NameToLayer ("delete_area");
+		if(c.gameObject.layer == layer){
+			Destroy(gameObject);
+		}
+	}
+}
